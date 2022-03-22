5e631b74a38a4aa59cd16cf027e6a9efc5ace3cb
qifeng <2713247954@qq.com>
2022-03-22 18:10:13 +0800

    员工详情页面完善

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -31,10 +31,21 @@
             </el-form>
           </el-tab-pane>
           <el-tab-pane label="个人详情">
-            <!-- 放置内容 -->
+            <!-- 放置内容 => 个人详情 -->
+            <!-- <user-info></user-info> -->
+            <!-- vuejs中内置了一个组件 component 可以是任意组件 作用：动态组件，可以切换组件 -->
+            <!-- <el-button type="primary" @click="UserComponent = 'el-table'"
+              >切换组件</el-button
+            >
+            <el-button type="primary" @click="UserComponent = 'user-info'"
+              >返回组件</el-button
+            > -->
+
+            <component :is="UserComponent" />
           </el-tab-pane>
           <el-tab-pane label="岗位信息">
-            <!-- 放置内容 -->
+            <!-- 放置内容 => 岗位信息 -->
+            <component :is="JobComponent" />
           </el-tab-pane>
         </el-tabs>
       </el-card>
@@ -45,9 +56,14 @@
 <script>
 import { getUserDetailById } from "@/api/user";
 import { saveUserDetailById } from "@/api/employees";
+import UserInfo from "./components/user-info"; // 引入子组件 个人详情
+import JobInfo from "./components/job-info"; // 引入子组件 岗位详情
 export default {
+  components: { UserInfo, JobInfo }, // 注册子组件
   data() {
     return {
+      UserComponent: UserInfo,
+      JobComponent: JobInfo,
       userId: this.$route.params.id, // 直接将路由中的参数赋值给了data中的属性
       userInfo: {
         username: "",
